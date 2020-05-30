3408b4a25b1c916b1345cd2155c9df02ca756822
Abhinav Dubey <abhinavdubey616@gmail.com>
2020-05-30 14:41:32 +0530

    Add files via upload

--- ModelTrain.py
+++ ModelTrain.py
@@ -25,8 +25,8 @@ callbacks = myCallback()
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
 
 # building the input vector from the 28x28 pixels
-X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
-X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+X_train = X_train.reshape(-1, 28, 28, 1)
+X_test = X_test.reshape(-1, 28, 28, 1)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
